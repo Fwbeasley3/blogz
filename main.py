2c17ca94f8a2839558063c5f18731d34c24c490d
Freddie Beasley <fwbeasley3@yahoo.com>
2019-04-30 20:33:01 -0500

    Final commit w/ hashing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, render_template,flash,session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from hashutils import make_pw_hash, check_pw_hash
 import re
 
 
@@ -44,12 +45,12 @@ class Blog(db.Model):
 class User(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(25),unique=True)
-    password = db.Column(db.String(25))
+    pw_hash = db.Column(db.String(120))
     blogs = db.relationship('Blog', backref='owner' )
 
     def __init__(self,username,password):
         self.username = username
-        self.password = password
+        self.pw_hash = make_pw_hash(password)
 
 def emptyField(str):    #check empty str
     if str !="":
@@ -97,7 +98,7 @@ def login():
         users = User.query.filter_by(username=username)
         if users.count() == 1:
             user = users.first()
-            if password == user.password:
+            if user and check_pw_hash(password, user.pw_hash):
                 session['username'] = user.username
                 flash('Welcome back, '+ user.username + '!')
                 return redirect('/newpost')
